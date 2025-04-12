b043622d5504ede5747b1ce23dba26cb39b1c26e
eternity2745 <vsdevan2006@gmail.com>
2025-04-12 15:37:20 +0530

    refactor: integrate ChangeNotifierProvider for state management in MyApp

--- authorityapp/lib/main.dart
+++ authorityapp/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:authorityapp/Screens/login.dart';
+import 'package:authorityapp/Utilities/state.dart';
 import 'package:authorityapp/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
 import 'package:sizer/sizer.dart';
 
@@ -18,21 +20,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-      builder: (p0, p1, p2) {
-        return MaterialApp(
-          title: 'Civic Authority',
-          darkTheme: ThemeData.dark(),
-          // highContrastDarkTheme: ThemeData.dark(),
-          theme: ThemeData(
-      
-            // primaryColor: Colors.green.shade900,
-            useMaterial3: true,
-          ),
-          debugShowCheckedModeBanner: false,
-          home: const LoginScreen(),
-        );
-      }
+    return ChangeNotifierProvider(
+      create: (context) => StateManagement(),
+      child: Sizer(
+        builder: (p0, p1, p2) {
+          return MaterialApp(
+            title: 'Civic Authority',
+            darkTheme: ThemeData.dark(),
+            // highContrastDarkTheme: ThemeData.dark(),
+            theme: ThemeData(
+        
+              // primaryColor: Colors.green.shade900,
+              useMaterial3: true,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: const LoginScreen(),
+          );
+        }
+      ),
     );
   }
 }
